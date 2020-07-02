7fac0b6ef98eab7e184ae9fdf0f6c5dd41a35d2e
Drvikas2021 <67689839+Drvikas2021@users.noreply.github.com>
2020-07-02 15:07:47 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,24 @@
+<?php
+$name=$_REQUEST['name'];
+$phone=$_REQUEST['phone'];
+$email=$_REQUEST['email'];
+$text=$_REQUEST['text'];
+
+if(empty($name)|| empty($phone) || empty($email) || empty($text))
+{
+	echo "Please fill all the fields";
+}
+else
+{
+	mail("vikashdeepak2014@gmail.com", "Client Querry", $message, "From: $name <
+	$email>");
+	echo "<script type='text/javascript'>alert('Thanks! we will contact you soon');
+	window.history.log(-1);
+	</script>";
+	
+}
+
+
+
+
+?>
