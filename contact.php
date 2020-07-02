b6705ca8d0089a561d26ce563671037ad1e83d92
Drvikas2021 <67689839+Drvikas2021@users.noreply.github.com>
2020-07-02 15:36:08 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -13,7 +13,7 @@ else
 	mail("vikashdeepak2014@gmail.com", "Client Querry", $message, "From: $name <
 	$email>");
 	echo "<script type='text/javascript'>alert('Thanks! we will contact you soon');
-	window.history.log(-1);
+	window.open('index.html','_self')
 	</script>";
 	
 }
