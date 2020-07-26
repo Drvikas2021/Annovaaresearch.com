4909cbef6512e70002bf078b4483c8880e5b0a27
Drvikas2021 <67689839+Drvikas2021@users.noreply.github.com>
2020-07-26 23:17:44 +0530

    Add files via upload

---
+++ includes/script1.js
@@ -0,0 +1,361 @@
+window.Apex = {
+  chart: {
+    foreColor: '#ccc',
+    toolbar: {
+      show: false
+    },
+  },
+  stroke: {
+    width: 3
+  },
+  dataLabels: {
+    enabled: false
+
+  },
+  
+  tooltip: {
+    theme: 'dark'
+  },
+  grid: {
+    borderColor: "#535A6C",
+    xaxis: {
+      lines: {
+        show: true
+      }
+    }
+  }
+};
+
+var spark1 = {
+  chart: {
+    id: 'spark1',
+    group: 'sparks',
+    type: 'line',
+    height: 80,
+    sparkline: {
+      enabled: true
+    },
+    dropShadow: {
+      enabled: true,
+      top: 1,
+      left: 1,
+      blur: 2,
+      opacity: 0.2,
+    }
+  },
+  series: [{
+    data: [1193078, 1238798, 1288108, 1337024, 1385635, 1424202]
+  }],
+  stroke: {
+    curve: 'smooth'
+  },
+  markers: {
+    size: 0
+  },
+  grid: {
+    padding: {
+      top: 20,
+      bottom: 10,
+      left: 110
+    }
+  },
+  labels: ['01/15/2002', '01/16/2002', '01/17/2002', '01/18/2002', '01/19/2002', '01/20/2002'],
+  colors: ['#fff'],
+  tooltip: {
+    x: {
+      show: false
+    },
+	 
+    y: {
+      title: {
+        formatter: function formatter(val) {
+          return 'Infected';
+        }
+      }
+    }
+  }
+}
+
+var spark2 = {
+  chart: {
+    id: 'spark2',
+    group: 'sparks',
+    type: 'line',
+    height: 80,
+    sparkline: {
+      enabled: true
+    },
+    dropShadow: {
+      enabled: true,
+      top: 1,
+      left: 1,
+      blur: 2,
+      opacity: 0.2,
+    }
+  },
+  series: [{
+    data: [753050, 782607, 817209, 849432, 885573, 885577]
+  }],
+  stroke: {
+    curve: 'smooth'
+  },
+  grid: {
+    padding: {
+      top: 20,
+      bottom: 10,
+      left: 110
+    }
+  },
+  markers: {
+    size: 0
+  },
+  colors: ['#fff'],
+  tooltip: {
+    x: {
+      show: false
+    },
+    y: {
+      title: {
+        formatter: function formatter(val) {
+          return 'Recovered';
+        }
+      }
+    }
+  }
+}
+
+var spark3 = {
+  chart: {
+    id: 'spark3',
+    group: 'sparks',
+    type: 'line',
+    height: 80,
+    sparkline: {
+      enabled: true
+    },
+    dropShadow: {
+      enabled: true,
+      top: 1,
+      left: 1,
+      blur: 2,
+      opacity: 0.2,
+    }
+  },
+  series: [{
+    data: [28732, 29861, 30601, 31358, 32060, 32063]
+  }],
+  stroke: {
+    curve: 'smooth'
+  },
+  markers: {
+    size: 0
+  },
+  grid: {
+    padding: {
+      top: 20,
+      bottom: 10,
+      left: 110
+    }
+  },
+  colors: ['#fff'],
+  xaxis: {
+    crosshairs: {
+      width: 1
+    },
+  },
+  tooltip: {
+    x: {
+      show: false
+    },
+    y: {
+      title: {
+        formatter: function formatter(val) {
+          return 'Mortality';
+        }
+      }
+    }
+  }
+}
+
+
+new ApexCharts(document.querySelector("#spark1"), spark1).render();
+new ApexCharts(document.querySelector("#spark2"), spark2).render();
+new ApexCharts(document.querySelector("#spark3"), spark3).render();
+
+
+
+var optionsLine = {
+  chart: {
+    height: 328,
+    type: 'line',
+	
+    zoom: {
+      enabled: false
+    },
+    dropShadow: {
+      enabled: true,
+      top: 3,
+      left: 2,
+      blur: 4,
+      opacity: 1,
+    }
+  },
+  stroke: {
+    curve: 'smooth',
+    width: 2
+  },
+  //colors: ["#3F51B5", '#2196F3'],
+  series: [{
+      name: "Confirmed",
+      data: [1193078, 1238798, 1288108, 1337024, 1385635, 1424202]
+    },
+    {
+      name: "Recovered",
+      data: [753050, 782607, 817209, 849432, 885573, 885577]
+    },
+    {
+      name: "Mortality",
+      data: [28732, 29861, 30601, 31358, 32060, 32063]
+    }
+  ],
+  title: {
+    text: 'Corona',
+    align: 'left',
+    offsetY: 25,
+    offsetX: 20
+  },
+  subtitle: {
+    text: 'Statistics',
+    offsetY: 55,
+    offsetX: 20
+  },
+  markers: {
+    size: 6,
+    strokeWidth: 0,
+    hover: {
+      size: 9
+    }
+  },
+  grid: {
+    show: true,
+    padding: {
+      bottom: 0
+    }
+  },
+  labels: ['21/07/2020', '22/07/2020', '23/07/2020', '24/07/2020', '25/07/2020', '26/07/2020'],
+  xaxis: {
+    tooltip: {
+      enabled: false
+    }
+  },
+  legend: {
+    position: 'top',
+    horizontalAlign: 'right',
+    offsetY: -20
+  }
+}
+
+var chartLine = new ApexCharts(document.querySelector('#line-adwords'), optionsLine);
+chartLine.render();
+
+
+
+
+var optionsBar = {
+  chart: {
+    height: 380,
+    type: 'bar',
+    stacked: true,
+  },
+  plotOptions: {
+    bar: {
+      columnWidth: '30%',
+      horizontal: false,
+    },
+  },
+  series: [{
+    name: 'PRODUCT A',
+    data: [14, 25, 21, 17, 12, 13, 11, 19]
+  }, {
+    name: 'PRODUCT B',
+    data: [13, 23, 20, 8, 13, 27, 33, 12]
+  }, {
+    name: 'PRODUCT C',
+    data: [11, 17, 15, 15, 21, 14, 15, 13]
+  }],
+  xaxis: {
+    categories: ['2011 Q1', '2011 Q2', '2011 Q3', '2011 Q4', '2012 Q1', '2012 Q2', '2012 Q3', '2012 Q4'],
+  },
+  fill: {
+    opacity: 1
+  },
+
+}
+
+var chartBar = new ApexCharts(
+  document.querySelector("#barchart"),
+  optionsBar
+);
+
+chartBar.render();
+
+var optionsArea = {
+  chart: {
+    height: 380,
+    type: 'area',
+    stacked: false,
+  },
+  stroke: {
+    curve: 'straight'
+  },
+  series: [{
+      name: "Music",
+      data: [11, 15, 26, 20, 33, 27]
+    },
+    {
+      name: "Photos",
+      data: [32, 33, 21, 42, 19, 32]
+    },
+    {
+      name: "Files",
+      data: [20, 39, 52, 11, 29, 43]
+    }
+  ],
+  xaxis: {
+    categories: ['2011 Q1', '2011 Q2', '2011 Q3', '2011 Q4', '2012 Q1', '2012 Q2'],
+  },
+  tooltip: {
+    followCursor: true
+  },
+  fill: {
+    opacity: 1,
+  },
+
+}
+
+var chartArea = new ApexCharts(
+  document.querySelector("#areachart"),
+  optionsArea
+);
+
+chartArea.render();
+
+/* fusionexport integrations START */
+(() => {
+    const btn = document.getElementById('fusionexport-btn')
+    btn.addEventListener('click', async function() {
+      const endPoint = 'https://www.fusioncharts.com/demos/dashboards/fusionexport-apexcharts/api/export-dashboard'
+      const information = {
+        dashboardName: 'dark'
+      };
+
+      this.setAttribute('disabled', true);
+      const { data } = await axios.post(endPoint, information, {
+        responseType: 'blob'
+      });
+      await download(data, 'apexCharts-dark-dashboard.pdf', 'application/pdf')
+      this.removeAttribute('disabled')
+    });
+  }
+)();
+/* fusionexport integrations END */
